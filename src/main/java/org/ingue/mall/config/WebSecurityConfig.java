7113ca8565e6d0b6df97e783121d504d7f195aca
mollder <mollder@naver.com>
2019-03-27 23:14:24 +0900

    #8 Spring Security 기본 설정

---
+++ src/main/java/org/ingue/mall/config/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package org.ingue.mall.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+}
