48a2ecb921a406c84f5d2e57eeaffabcb1acdfd0
mollder <mollder@naver.com>
2019-04-01 16:57:47 +0900

    #9 spring security 로그인 페이지 url 설정

--- src/main/java/org/ingue/mall/config/WebSecurityConfig.java
+++ src/main/java/org/ingue/mall/config/WebSecurityConfig.java
@@ -1,9 +1,27 @@
 package org.ingue.mall.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class WebSecurityConfig implements WebMvcConfigurer {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST = {
+            "/",
+            "/static/**",
+            "/dist/**",
+    };
+
+    protected void configure(HttpSecurity http) throws Exception {
+         http
+            .authorizeRequests()
+                 .antMatchers(AUTH_WHITELIST).permitAll()
+                 .antMatchers("/success/**").hasRole("USER")
+                 .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/")
+                .permitAll();
+    }
 }
