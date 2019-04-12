6956c21735f2e46c53b7ef13df3fa93331c10455
mollder <mollder@naver.com>
2019-04-12 12:36:42 +0900

    #17 게시판 뷰 작업 : 페이징, 검색, 드롭다운, 글쓰기 버튼 추가

--- frontend/src/components/Layout.vue
+++ frontend/src/components/Layout.vue
@@ -13,9 +13,45 @@
       </b-navbar-nav>
     </b-navbar>
 
-    <b-table :items="items" :fields="fields"></b-table>
+    <b-table :items="items" :fields="fields" id="board-table"></b-table>
 
+    <b-row>
+      <b-col>
+        <b-row>
+          <b-col>
+            <b-dropdown id="dropdown-1" text="제목+내용" class="m-md-2">
+              <b-dropdown-item>제목+내용</b-dropdown-item>
+              <b-dropdown-item>글제목</b-dropdown-item>
+              <b-dropdown-item>글쓴이</b-dropdown-item>
+              <b-dropdown-item>글번호</b-dropdown-item>
+              <b-dropdown-item>댓글 내용</b-dropdown-item>
+              <b-dropdown-item>댓글 작성자</b-dropdown-item>
+            </b-dropdown>
+          </b-col>
 
+          <b-col>
+            <b-form-input></b-form-input>
+          </b-col>
+
+          <b-col>
+            <b-button variant="outline-primary">검색</b-button>
+          </b-col>
+        </b-row>
+      </b-col>
+
+      <b-col>
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="perPage"
+          aria-controls="board-table"
+        ></b-pagination>
+      </b-col>
+
+      <b-col>
+        <b-button variant="outline-primary">글쓰기</b-button>
+      </b-col>
+    </b-row>
 
   </div>
 </template>
@@ -32,9 +68,15 @@
               { writingNum: 14315, title: '호그와트 시험기간 애브리타임', writer: '까륵', date: '19-04-10', view: 14312, up: 0 },
               { writingNum: 14314, title: '의정부고에 세워진 동상', writer: '선녀와난후끈', date: '19-04-10', view: 19460, up: 0 },
             ],
+            currentPage: 1,
+            perPage: 10,
           };
       },
-
+      computed: {
+        rows() {
+          return this.items.length
+        }
+      }
     }
 </script>
 
